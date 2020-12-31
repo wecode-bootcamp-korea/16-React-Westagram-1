d66fae6ecfc7e4a49f90300b1ec5cbcaa59c4be5
Taeng <dyek72731@gmail.com>
2020-12-31 12:39:27 +0900

    Modify: index.js 내 Routes 반영, 미사용 파일삭제

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LoginTaeHyung from './Pages/taehyungkim/Login/Login';
 import MainTaeHyung from './Pages/taehyungkim/Main/Main';
