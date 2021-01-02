2efda93812e26abbb0246cf64c0d83a25611f2ea
김승환 <hwan960528@gmail.com>
2021-01-02 14:31:45 +0900

    Modify: Enter key로 로그인 버튼 실행되게 하기

--- src/Pages/seunghwankim/Login/Login.js
+++ src/Pages/seunghwankim/Login/Login.js
@@ -22,8 +22,18 @@ class Login extends React.Component {
         })
     }
 
-    showLoginInfo = (e) => {
+    validLoginInfo = () => {
+        return (this.state.id.includes('@')) && (this.state.pw.trim().length >= 5) ? true : false
+    }
+
+    showLoginInfo = () => {
         console.log(`{id: ${this.state.id} pw: ${this.state.pw}}`)
+    }
+
+    loginByEnterKey = (e) => {
+        if(e.key === 'Enter' && this.validLoginInfo()) {
+            document.getElementsByTagName('a')[0].click();
+        }
     }
 
     showPassword = () => {
@@ -38,12 +48,12 @@ class Login extends React.Component {
                         <h1>Westagram</h1>
                     </div>
                     <div className="input_container">
-                        <input id="id" onChange={this.handleLoginInfo} value={this.state.id} type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+                        <input id="id" onChange={this.handleLoginInfo} onKeyDown={this.loginByEnterKey} value={this.state.id} type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
                         <div className="pw_container">
-                            <input id="pw" onChange={this.handleLoginInfo} value={this.state.pw} type={this.state.hiddenPw ? "password" : "text"} placeholder="비밀번호" />
+                            <input id="pw" onChange={this.handleLoginInfo} onKeyDown={this.loginByEnterKey} value={this.state.pw} type={this.state.hiddenPw ? "password" : "text"} placeholder="비밀번호" />
                             <span className="showPw" onClick={this.showPassword}>{this.state.hiddenPw ? "show" : "hide"}</span>
                         </div>
-                        <Link className={(this.state.id.trim().includes('@')) && (this.state.pw.trim().length >= 5) ? "" : "disable" } to={(this.state.id.trim().includes('@')) && (this.state.pw.trim().length >= 5) ? "/main-seunghwan" : "/login-seunghwan" } onClick={this.showLoginInfo}>로그인</Link>
+                        <Link className={this.validLoginInfo() ? "" : "disable" } to={this.validLoginInfo() ? "/main-seunghwan" : "/login-seunghwan" } onClick={this.validLoginInfo() ? this.showLoginInfo : undefined}>로그인</Link>
                     </div>
                 </div>
             </div>
